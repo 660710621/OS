7269b71919034abca11258b02e5047c10d35e465
0MGiant <tanadol.supa@gmail.com>
2025-08-17 18:23:40 +0700

    demo&Process
    -Change Demo
    -Change Process

--- Process.java
+++ Process.java
@@ -1,6 +1,17 @@
 public class Process {
-    int PID;
-    Process(int PID){
+    private int PID;
+    private boolean isBoss;
+    Process(int PID,boolean isBoss){
         this.PID = PID;
+        this.isBoss = isBoss;
+    }
+    public int getPID() {
+        return PID;
+    }
+    public boolean getisBoss(){
+        return isBoss;
+    }
+    public void setBoss(boolean isBoss) {
+        this.isBoss = isBoss;
     }
 }
