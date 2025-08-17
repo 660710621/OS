1e26b78dd45a8b1363b63134bb0ed82ac77558f9
0MGiant <tanadol.supa@gmail.com>
2025-08-17 22:22:26 +0700

    edit random num

--- Demo.java
+++ Demo.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -6,18 +9,25 @@ public class Demo {
         Scanner scan = new Scanner(System.in);
         int n = scan.nextInt();
         Process[] process = new Process[n];
-        
-        for(int i = 0; i < n; i++){
-            Random random = new Random();
-            int PID = random.nextInt(100); 
-            process[i] = new Process(PID,false);
+        //สร้างตัว PID Pool ขึ้นมาเพื่อเก็บเลข 0 - 99
+        List<Integer> pidPool = new ArrayList<>();
+        for (int i = 0; i < 100; i++){
+            pidPool.add(i);
+            System.out.println(i);
+        }
+            
+        Collections.shuffle(pidPool);
+
+        for (int i = 0; i < n; i++) {
+            int PID = pidPool.get(i);
+            process[i] = new Process(PID, false);
             System.out.println("Process " + i + " มี PID = " + PID);
         }
         // หา PID สูงสุด
-        int maxPID = process[0].getPID(); 
+        int maxPID = process[0].getPID();
         int maxIndex = 0;
-        for(int i = 1; i < n; i++){
-            if(process[i].getPID() > maxPID){
+        for (int i = 1; i < n; i++) {
+            if (process[i].getPID() > maxPID) {
                 maxPID = process[i].getPID();
                 maxIndex = i;
             }
