b9a96994c009b07b53b705421fae6e7ea4fc2524
0MGiant <tanadol.supa@gmail.com>
2025-09-16 12:41:38 +0700

    change try except

--- process_node.py
+++ process_node.py
@@ -4,58 +4,57 @@ import redis
 import json
 
 class ProcessNode:
-    def __init__(self, pid):
-        self.pid = pid
-        self.members = {}
-        self.boss_id = None
-        self.redis = redis.Redis(host='localhost', port=6379, db=0)
-        self.channel = "Heartbeat"
+    def __init__(process, pid):
+        process.pid = pid
+        process.members = {}
+        process.boss_id = None
+        process.redis = redis.Redis(host='localhost', port=6379, db=0)
+        process.channel = "Heartbeat"
 
-    def start(self):
-        threading.Thread(target=self.heartbeat_sender, daemon=True).start()
-        threading.Thread(target=self.heartbeat_Listener, daemon=True).start()
-        threading.Thread(target=self.failure_detectorAndBoss_Election, daemon=True).start()
+    def start(process):
+        threading.Thread(target=process.heartbeat_sender, daemon=True).start()
+        threading.Thread(target=process.heartbeat_Listener, daemon=True).start()
+        threading.Thread(target=process.failure_detectorAndBoss_Election, daemon=True).start()
 
     # ส่ง heartbeat ทุก 1 วินาที
-    def heartbeat_sender(self):
+    def heartbeat_sender(process):
         while True:
-            msg = json.dumps({"pid": self.pid,"status":"alive"})
-            self.redis.publish(self.channel, msg)
+            msg = json.dumps({"pid": process.pid, "status": "alive"})
+            process.redis.publish(process.channel, msg)
             time.sleep(1)
 
     # ฟัง heartbeat จาก Redis
-    def heartbeat_Listener(self):
-        pubsub = self.redis.pubsub()
-        pubsub.subscribe(self.channel)
+    def heartbeat_Listener(process):
+        pubsub = process.redis.pubsub()
+        pubsub.subscribe(process.channel)
 
         for message in pubsub.listen():
             if message['type'] != 'message':
                 continue
-            try:
-                data = json.loads(message['data'])
-                sender_pid = data["pid"]
-                if sender_pid != self.pid:  # ไม่รับของตัวเอง
-                    self.members[sender_pid] = time.time()
-            except:
-                pass
+
+            data = json.loads(message['data'])
+            sender_pid = data["pid"]
+            if sender_pid != process.pid:  # ไม่รับของตัวเอง
+                process.members[sender_pid] = time.time()
+
 
     # ตรวจสอบ failure และเลือก Boss
-    def failure_detectorAndBoss_Election(self):
+    def failure_detectorAndBoss_Election(process):
         while True:
             now = time.time()
-            alive_pids = [pid for pid, t in self.members.items() if now - t < 20]
-            alive_pids.append(self.pid)
+            alive_pids = [pid for pid, t in process.members.items() if now - t < 20]
+            alive_pids.append(process.pid)
 
             dead_pids = []
-            for pid, t in self.members.items():
+            for pid, t in process.members.items():
                 if now - t >= 20:  # ถ้า heartbeat ล่าสุดเกิน 20 วินาที
                     dead_pids.append(pid)
 
             if dead_pids:
                 print(f" Dead PIDs detected: {dead_pids}")
 
-            self.boss_id = max(alive_pids)
+            process.boss_id = max(alive_pids)
 
             print("Members List :",alive_pids)
-            print(f"PID {self.pid}: Current Boss is {self.boss_id}")
+            print(f"PID {process.pid}: Current Boss is {process.boss_id}")
             time.sleep(3)
